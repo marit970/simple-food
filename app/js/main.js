c8248d2c72281aa5021c3f82ce11044097058ef5
marit970 <43189340+marit970@users.noreply.github.com>
2024-07-07 17:54:27 +0200

    add menu-active

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,10 @@
+/* eslint-disable no-undef */
+$('.menu__link').on('click', function (event) {
+  event.preventDefault();
+
+  $('.menu__link').removeClass('menu__link--active');
+  $(this).addClass('menu__link--active');
+});
+
 // eslint-disable-next-line no-undef
 mixitup('.popular-categories__nav-list');
