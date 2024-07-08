5a155ead67af8bcbe63807154ef99d1f06c44b0f
marit970 <43189340+marit970@users.noreply.github.com>
2024-07-09 00:06:05 +0200

    add mobile

--- app/js/main.js
+++ app/js/main.js
@@ -23,5 +23,24 @@ $('.testimonials__content').slick({
   infinite: false,
 });
 
+// start mobile slider for popular-categories
+if (window.matchMedia('(min-width: 400px)').matches) {
+  /* the viewport is at least 400 pixels wide */
+} else {
+  /* the viewport is less than 400 pixels wide */
+}
+
+//DOMContentLoaded означает, когда наш документ будет готов к работе, тогда начнут работать наши скрипты
+
+//Mobile Menu
+const burger = document.querySelector('.burger'); //находим наш бургер по селектору класса
+
+burger.addEventListener('click', () => {
+  //Добавляем событие "клик" на бургер
+
+  burger.classList.toggle('burger--active'); //при клике на бургер у нас будет либо добавлятся класс, либо удаляется.
+  //ВАЖНО! Мы уже работаем с данным классом, поэтому тут "." не ставим, иначе в атрибут class значение добавится с "." и работать не будет.
+});
+
 // eslint-disable-next-line no-undef
 mixitup('.popular-categories__list');
